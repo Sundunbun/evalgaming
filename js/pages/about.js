58c5072baa89d492334c8baa3f62866e7390d6e1
Sundunbun <zekoualcide@gmail.com>
2025-03-05 00:19:44 -0500

    Removed old files from eval2 while keeping latest site

--- js/pages/about.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const basePath = window.getBasePath();
-    document.getElementById('joinNowButton').href = `${basePath}pages/signup.html`;
-});

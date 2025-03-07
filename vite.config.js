1479ef865c99a31d067f2845f91e3df0707c24a1
charlespers <charlespers.m@gmail.com>
2025-03-06 23:17:51 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/eval2', // 👈 Use your repo name here if hosted on GitHub Pages
+  base: '/', // 👈 Use your repo name here if hosted on GitHub Pages
   build: {
     outDir: "dist",
   },
